66219ae67015b574e17c6dbc53bd16869a29be6e
HalfWing <halfwing1998@gmail.com>
2021-10-26 23:09:03 +0900

    third

---
+++ counting1/index.js
@@ -0,0 +1,13 @@
+const { oidd,even, odd} = require('./var');
+const checkNumber = require('./func');
+
+function checkStringOddOrEven(str){
+    if(str.length%2){
+        return odd;
+    }
+    return even;
+}
+
+console.log(checkNumber(10));
+
+console.log(checkStringOddOrEven('hello'));
