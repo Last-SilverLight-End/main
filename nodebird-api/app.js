cee17f8d1075ac96e88a1145528d840665521ba4
HalfWing_lab <halfwing1998@gmail.com>
2021-11-09 22:00:37 +0900

    20211109 453p temp1

--- nodebird-api/app.js
+++ nodebird-api/app.js
@@ -33,3 +33,27 @@ sequelize.sync({ force : false})
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave : false,
+    saveUninitialized: false,
+    secret : process.env.COOKIE_SECRET,
+    cookie : {
+        httpOnly: true,
+        secure:false,
+    },
+}));
+app.use(passpoert.initialize());
+app.use(passport.session());
+
+app.use('/auth',authRouter);
+app.use('/',indexRouter);
+
+app.use((req,res,next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+    error.status = 404;
+    next(error);
+});
+
